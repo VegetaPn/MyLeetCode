a304ba210f5b0a991e5137c28ee3d1aa2251293e
延昊南 <yhnkb@hotmail.com>
2016-08-11 14:36:06 +0800

    62. Unique Paths

---
+++ unique_paths.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int uniquePaths(int m, int n) {
+
+        int[][] pathMap = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            pathMap[i][0] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            pathMap[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                pathMap[i][j] = pathMap[i-1][j] + pathMap[i][j-1];
+
+        return pathMap[m-1][n-1];
+    }
+}
