24c96b16f35a5406cc351067f6a475c73aa44489
延昊南 <yhnkb@hotmail.com>
2016-09-08 09:32:00 +0800

    324. Wiggle Sort II

--- wiggle_sort_2.py
+++ wiggle_sort_2.py
@@ -8,17 +8,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        if len(nums) <= 2:
-            return nums
         nums.sort()
-        i = 1
-        l = len(nums)
-        while i < l - 1:
-            print(i)
-            t = nums[i]
-            nums[i] = nums[i+1]
-            nums[i+1] = t
-            i += 2
+        if len(nums) <= 2:
+            return
+        half = len(nums[::2])
+        nums[::2], nums[1::2] = nums[:half][::-1], nums[half:][::-1]
 
 
 if __name__ == '__main__':
